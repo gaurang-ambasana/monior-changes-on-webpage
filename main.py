93a09d47a104102d3455b94a9e6b9511cf348bb2
Gaurang Ambasana <gpambasana@gmail.com>
2021-11-27 17:45:40 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,107 @@
+from time import sleep as delay
+from datetime import datetime as timestamp
+from bs4 import BeautifulSoup as prepare_beautiful_soup
+from difflib import context_diff
+from difflib import Differ as differentiator
+from requests import get as make_get_request
+from send_email import main_send_email as send_alert
+
+url = "http://127.0.0.1:5500/index.html"
+prev_version = ""
+first_run = True
+flag = True
+
+
+def get_plain_str(iterator_str):
+    return "\n".join([line.rstrip() for line in '\n'.join(iterator_str).splitlines() if line.strip()])
+
+
+def compare_content(old_content, new_content):
+    return context_diff(old_content, new_content)
+
+
+def compare_entire_contect(old_content, new_content):
+    return differentiator().compare(old_content, new_content)
+
+
+def create_changelog_file(difference):
+    current_time = str(timestamp.now()).replace(" ", "_")
+
+    text = '--> Changes detected at : ' + current_time + \
+        ' for webpage :' + url + '\n\n' + difference + \
+        '\n\n---------------------------------------------------\n\n'
+
+    file_path = "F:\web development\monior-changes-on-webpage\changelog.txt"
+
+    with open(file_path, 'r') as file:
+        og_data = file.read()
+
+    with open(file_path, 'w') as file:
+        file.write(text + og_data)
+
+    print("-> Changelog updated!")
+
+
+def get_soup_without_script_n_style(html_data):
+    soup = prepare_beautiful_soup(html_data.text, "lxml")
+
+    tags_to_exclude = ["script", "style"]
+
+    for tag in soup(tags_to_exclude):
+        tag.extract()
+
+    new_soup = soup.get_text()
+
+    return new_soup
+
+
+def show_error_msg(error):
+    print("Request failed for URL : " + url + "\nReason : " + str(error))
+
+
+def send_GET_request_like_browser(url):
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+    res = make_get_request(url, headers=headers)
+
+    return res
+
+
+while flag:
+
+    try:
+        response = send_GET_request_like_browser(url)
+    except Exception as ex:
+        show_error_msg(ex)
+        flag = False
+        break
+
+    res_soup = get_soup_without_script_n_style(response)
+
+    if prev_version != res_soup:
+        if first_run == True:
+            first_run = False
+            prev_version = res_soup
+
+            print("Now, Monitoring " + url + " at " + str(timestamp.now()))
+        else:
+            print("Changes detected at: " + str(timestamp.now()))
+
+            old_page = prev_version.splitlines()
+            new_page = res_soup.splitlines()
+
+            entire_page_difference = compare_entire_contect(old_page, new_page)
+            only_different_content = compare_content(old_page, new_page)
+
+            difference_str = get_plain_str(only_different_content)
+            page_difference_str = get_plain_str(entire_page_difference)
+
+            create_changelog_file(page_difference_str)
+            print(difference_str)
+
+            old_page, prev_version = new_page, res_soup
+    else:
+        print("No Changes Detected till : " + str(timestamp.now()))
+
+    delay(5)
+    continue
---
+++ send_email.py
@@ -0,0 +1,188 @@
+import httplib2
+import os
+import oauth2client
+from oauth2client import client, tools, file
+import base64
+import mimetypes
+from email.mime.audio import MIMEAudio
+from email.mime.base import MIMEBase
+from email.mime.image import MIMEImage
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.application import MIMEApplication
+from apiclient import errors, discovery
+
+
+def get_credentials():
+
+    home_dir = os.path.expanduser('~')
+
+    credential_dir = os.path.join(home_dir, '.credentials')
+
+    if not os.path.exists(credential_dir):
+        os.makedirs(credential_dir)
+
+    credential_path = os.path.join(credential_dir, 'creds.json')
+
+    store = oauth2client.file.Storage(credential_path)
+    credentials = store.get()
+
+    if not credentials or credentials.invalid:
+        CLIENT_SECRET_FILE = 'credentials.json'
+        APPLICATION_NAME = 'Gmail API Python Send Email'
+
+        SCOPES = 'https://www.googleapis.com/auth/gmail.send'
+
+        flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
+        flow.user_agent = APPLICATION_NAME
+
+        credentials = tools.run_flow(flow, store)
+
+    return credentials
+
+
+def create_message_and_send(sender, to, subject,  message_text_plain, message_text_html):
+    credentials = get_credentials()
+
+    http = httplib2.Http()
+
+    http = credentials.authorize(http)
+
+    service = discovery.build('gmail', 'v1', http=http)
+
+    message_without_attachment = create_message_without_attachment(
+        sender, to, subject, message_text_html, message_text_plain)
+
+    send_Message_without_attachment(
+        service, "me", message_without_attachment, message_text_plain)
+
+
+def create_message_without_attachment(sender, to, subject, message_text_html, message_text_plain):
+    message = MIMEMultipart('alternative')
+
+    message['Subject'] = subject
+    message['From'] = sender
+    message['To'] = to
+
+    message.attach(MIMEText(message_text_plain, 'plain'))
+    message.attach(MIMEText(message_text_html, 'html'))
+
+    raw_message_no_attachment = base64.urlsafe_b64encode(message.as_bytes())
+    raw_message_no_attachment = raw_message_no_attachment.decode()
+    body = {'raw': raw_message_no_attachment}
+
+    return body
+
+
+def create_Message_with_attachment(sender, to, subject, message_text_plain, message_text_html, attached_file):
+    message = MIMEMultipart()
+    message['to'] = to
+    message['from'] = sender
+    message['subject'] = subject
+
+    message.attach(MIMEText(message_text_html, 'html'))
+    message.attach(MIMEText(message_text_plain, 'plain'))
+
+    my_mimetype, encoding = mimetypes.guess_type(attached_file)
+
+    if my_mimetype is None or encoding is not None:
+        my_mimetype = 'application/octet-stream'
+
+    main_type, sub_type = my_mimetype.split('/', 1)
+
+    if main_type == 'text':
+        print("text")
+
+        temp = open(attached_file, 'r')
+        attachment = MIMEText(temp.read(), _subtype=sub_type)
+
+        temp.close()
+
+    elif main_type == 'image':
+        print("image")
+
+        temp = open(attached_file, 'rb')
+        attachment = MIMEImage(temp.read(), _subtype=sub_type)
+
+        temp.close()
+
+    elif main_type == 'audio':
+        print("audio")
+
+        temp = open(attached_file, 'rb')
+        attachment = MIMEAudio(temp.read(), _subtype=sub_type)
+
+        temp.close()
+
+    elif main_type == 'application' and sub_type == 'pdf':
+        temp = open(attached_file, 'rb')
+        attachment = MIMEApplication(temp.read(), _subtype=sub_type)
+
+        temp.close()
+
+    else:
+        attachment = MIMEBase(main_type, sub_type)
+
+        temp = open(attached_file, 'rb')
+        attachment.set_payload(temp.read())
+
+        temp.close()
+
+    filename = os.path.basename(attached_file)
+    attachment.add_header('Content-Disposition',
+                          'attachment', filename=filename)
+    message.attach(attachment)
+
+    message_as_bytes = message.as_bytes()
+    message_as_base64 = base64.urlsafe_b64encode(message_as_bytes)
+
+    raw = message_as_base64.decode()
+
+    return {'raw': raw}
+
+
+def send_Message_without_attachment(service, user_id, body, message_text_plain):
+    try:
+        message_sent = (service.users().messages().send(
+            userId=user_id, body=body).execute())
+        message_id = message_sent['id']
+
+        print(
+            f'Message sent (without attachment) \n\n Message Id: {message_id}\n\n Message:\n\n {message_text_plain}')
+
+    except errors.HttpError as error:
+        print(f'An error occurred: {error}')
+
+
+def send_message_with_attachment(service, user_id, message_with_attachment):
+    try:
+        message_sent = (service.users().messages().send(
+            userId=user_id, body=message_with_attachment).execute())
+        message_id = message_sent['id']
+
+    except errors.HttpError as error:
+        print(f'An error occurred: {error}')
+
+
+def main_send_email(sender, to, subject, message_text_html, attached_file=''):
+    message_text_plain = "HTML Not Supported!"
+
+    if attached_file != '':
+        credentials = get_credentials()
+
+        http = httplib2.Http()
+        http = credentials.authorize(http)
+
+        service = discovery.build('gmail', 'v1', http=http)
+
+        msg_with_attachment = create_Message_with_attachment(
+            sender, to, subject, message_text_plain, message_text_html, attached_file)
+
+        send_message_with_attachment(service, "me", msg_with_attachment)
+    else:
+        create_message_and_send(sender, to, subject,
+                                message_text_plain, message_text_html)
+
+
+main_send_email("gaurang.ambasana@dev2.wipro.com", "kevin.sanghavi1@dev2.wipro.com", "test mail",
+                "Hi<br/><br/>HTML MSG", "F:\web development\monior-changes-on-webpage\changelog.txt")
